03f25fbc9bbb8109897c0f0dc73306842e3d76b7
Shubham Chaurasia <82652474+shubh1118@users.noreply.github.com>
2025-08-21 23:28:41 +0530

    updated
    Signed-off-by: Shubham Chaurasia <82652474+shubh1118@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,26 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  // This tells ESLint to completely ignore all files within the src/generated/ directory
+
   {
     ignores: ["src/generated/"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: [
-      "src/pages/admin/add-artwork.tsx",
-      "src/pages/admin/edit-artwork/[artworkId].tsx",
-      "src/pages/art/[id].tsx",
-      "src/pages/index.tsx",
-      "src/shared-theme/ColorModeSelect.tsx",
-      "src/utils/AuthContext.tsx",
-    ],
+
+    files: ["**/*.{ts,tsx,js,jsx}"],
     rules: {
-      // Temporarily allows 'any' types in these files to pass the build.
       "@typescript-eslint/no-explicit-any": "off",
-      // Temporarily ignores unescaped entities in these files.
       "react/no-unescaped-entities": "off",
-      // Temporarily ignores unused variables in these files.
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
